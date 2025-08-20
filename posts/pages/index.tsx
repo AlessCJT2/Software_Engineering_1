3d81427479d22a47983137d3d468598d06a03355
Alessandro <alessxdxd123@gmail.com>
2025-08-19 19:37:00 -0600

    commit1: integrations of supabse, nexta and typescipt

---
+++ posts/pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Gestión de Publicaciones</h1>
+      <p>Bienvenido a la aplicación de posts con Supabase.</p>
+    </div>
+  );
+}
